e664532e3adc10e42ffa92db9507f92f8adc2fe8
Japan-Manul <hehhehov8@gmail.com>
2025-09-24 21:41:18 +0300

    style(functions): little comments style change

--- tests/functions/SqrFunctionTest.java
+++ tests/functions/SqrFunctionTest.java
@@ -11,8 +11,8 @@ class SqrFunctionTest {
         SqrFunction fun = new SqrFunction();
 
         assertEquals(25.0, fun.apply(5.0), 1e-10);
-        assertEquals(0.0, fun.apply(0.0), 1e-10);                       // Проверка на 0
-        assertEquals(10000.0, fun.apply(-100.0), 1e-10);                // Проверка отрицательного числа
-        assertEquals(0.25, fun.apply(0.5), 1e-10);                      // Проверка дробного числа
+        assertEquals(0.0, fun.apply(0.0), 1e-10);           // Проверка на 0
+        assertEquals(10000.0, fun.apply(-100.0), 1e-10);    // Проверка отрицательного числа
+        assertEquals(0.25, fun.apply(0.5), 1e-10);          // Проверка дробного числа
     }
 }
