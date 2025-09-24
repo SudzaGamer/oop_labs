651a9e79121201af47a118d26d82f7d0b0182421
Japan-Manul <hehhehov8@gmail.com>
2025-09-24 21:30:51 +0300

    test(functions): SqrFunctionTest

---
+++ tests/functions/SqrFunctionTest.java
@@ -0,0 +1,18 @@
+package tests.functions;
+
+import main.functions.SqrFunction;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SqrFunctionTest {
+
+    @org.junit.jupiter.api.Test
+    void apply() {
+        SqrFunction fun = new SqrFunction();
+
+        assertEquals(25.0, fun.apply(5.0), 1e-10);
+        assertEquals(0.0, fun.apply(0.0), 1e-10);                       // Проверка на 0
+        assertEquals(10000.0, fun.apply(-100.0), 1e-10);                // Проверка отрицательного числа
+        assertEquals(0.25, fun.apply(0.5), 1e-10);                      // Проверка дробного числа
+    }
+}
