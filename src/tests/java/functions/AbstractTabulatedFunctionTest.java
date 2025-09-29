e03727cdcc7050c73c9f7c91bae691d93fb58676
Japan-Manul <hehhehov8@gmail.com>
2025-09-29 21:43:43 +0300

    test(functions): AbstractTabulatedFunctionTest

---
+++ src/tests/java/functions/AbstractTabulatedFunctionTest.java
@@ -0,0 +1,28 @@
+package functions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbstractTabulatedFunctionTest {
+
+    @Test
+    void interpolate() {
+        MockTabulatedFunction fun = new MockTabulatedFunction();
+
+        assertEquals(6.0, fun.interpolate(1.5, 1.0, 2.0, 4.0, 8.0), 1e-10);
+        assertEquals(0.0, fun.interpolate(0.0, 1.0, 2.0, 4.0, 8.0), 1e-10);
+        assertEquals(12.0, fun.interpolate(3.0, 1.0, 2.0, 4.0, 8.0), 1e-10);
+    }
+
+    @Test
+    void apply() {
+        MockTabulatedFunction fun = new MockTabulatedFunction();
+
+        assertEquals(4.0, fun.apply(1.0), 1e-10);
+        assertEquals(8.0, fun.apply(2.0), 1e-10);           // Проверка на 0
+        assertEquals(6.0, fun.apply(1.5), 1e-10);    // Проверка отрицательного числа
+        assertEquals(0.0, fun.apply(0.0), 1e-10);          // Проверка дробного числа
+        assertEquals(12.0, fun.apply(3.0), 1e-10);          // Проверка дробного числа
+    }
+}
---
+++ src/tests/java/functions/MockTabulatedFunction.java
@@ -0,0 +1,80 @@
+package functions;
+
+public class MockTabulatedFunction extends AbstractTabulatedFunction{
+    private final double x0 = 1;
+    private final double x1 = 2;
+
+    private final double y0 = 4;
+    private final double y1 = 8;
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public double getX(int index) {
+        if (index == 0) return 1;
+        if (index == 1) return 2;
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public double getY(int index) {
+        if (index == 0) return 4;
+        if (index == 1) return 8;
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public void setY(int index, double value) {}
+
+    @Override
+    public int indexOfX(double x) {
+        if (x == 1) return 0;
+        if (x == 2) return 1;
+        return -1;
+    }
+
+    @Override
+    public int indexOfY(double y) {
+        if (y == 4) return 0;
+        if (y == 8) return 1;
+        return -1;
+    }
+
+    @Override
+    public double leftBound(){
+        return x0;
+    }
+
+    @Override
+    public double rightBound(){
+        return x1;
+    }
+
+    @Override
+    protected int floorIndexOfX(double x) {
+        if (x == 1) return 0;
+        if (x == 2) return 1;
+        if (x < 1) return 0;
+        if (x < 2) return 0;
+        return 2;
+    }
+
+    @Override
+    protected double extrapolateLeft(double x) {
+        return interpolate(x, x0, x1, y0, y1);
+    }
+
+    @Override
+    protected double extrapolateRight(double x) {
+        return interpolate(x, x0, x1, y0, y1);
+    }
+
+    @Override
+    protected double interpolate(double x, int floorIndex) {
+        return interpolate(x, x0, x1, y0, y1);
+    }
+
+}
