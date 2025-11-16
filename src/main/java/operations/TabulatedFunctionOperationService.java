1d7be109ecdf38186c1b1c713cb28d261dff6323
Japan-Manul <hehhehov8@gmail.com>
2025-11-16 20:55:15 +0300

    Operators add, subtract

--- src/main/java/operations/TabulatedFunctionOperationService.java
+++ src/main/java/operations/TabulatedFunctionOperationService.java
@@ -1,9 +1,30 @@
 package operations;
 
+import exceptions.InconsistentFunctionsException;
 import functions.Point;
 import functions.TabulatedFunction;
+import functions.factory.ArrayTabulatedFunctionFactory;
+import functions.factory.TabulatedFunctionFactory;
 
 public class TabulatedFunctionOperationService {
+    private TabulatedFunctionFactory factory;
+
+    TabulatedFunctionOperationService(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
+    TabulatedFunctionOperationService() {
+        this.factory = new ArrayTabulatedFunctionFactory();
+    }
+
+    public TabulatedFunctionFactory getFactory() {
+        return factory;
+    }
+
+    public void setFactory(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
     public static Point[] asPoints(TabulatedFunction tabulatedFunction) {
         int size = tabulatedFunction.getCount();
         Point[] points = new Point[size];
@@ -14,5 +35,42 @@ public class TabulatedFunctionOperationService {
             i++;
         }
         return points;
+    }
+
+    private interface BiOperation {
+        double apply(double u, double v);
+    }
+
+    private TabulatedFunction doOperation(TabulatedFunction a, TabulatedFunction b, BiOperation operation) {
+        int sizeA = a.getCount();
+        int sizeB = b.getCount();
+        if (sizeA != sizeB) {
+            throw new InconsistentFunctionsException("Different number of points");
+        }
+
+        Point[] pointsA = asPoints(a);
+        Point[] pointsB = asPoints(b);
+
+        double[] xValues = new double[sizeA];
+        double[] yValues = new double[sizeA];
+
+        for (int i = 0; i < sizeA; i++) {
+            if (pointsA[i].x != pointsB[i].x) {
+                throw new InconsistentFunctionsException("X-coordinates differ at index " + i);
+            }
+
+            xValues[i] = pointsA[i].x;
+            yValues[i] = operation.apply(pointsA[i].y, pointsB[i].y);
+        }
+
+        return factory.create(xValues, yValues);
+    }
+
+    public TabulatedFunction add(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u + v);
+    }
+
+    public TabulatedFunction subtract(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u - v);
     }
 }
--- src/tests/java/operations/TabulatedFunctionOperationServiceTest.java
+++ src/tests/java/operations/TabulatedFunctionOperationServiceTest.java
@@ -1,7 +1,11 @@
 package operations;
 
 import functions.ArrayTabulatedFunction;
+import functions.LinkedListTabulatedFunction;
 import functions.Point;
+import functions.TabulatedFunction;
+import functions.factory.ArrayTabulatedFunctionFactory;
+import functions.factory.LinkedListTabulatedFunctionFactory;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,11 +17,9 @@ class TabulatedFunctionOperationServiceTest {
         double[] xValues = {0.0, 1.0, 2.0};
         double[] yValues = {0.0, 1.0, 4.0};
 
-        ArrayTabulatedFunction function =
-                new ArrayTabulatedFunction(xValues, yValues);
+        ArrayTabulatedFunction function = new ArrayTabulatedFunction(xValues, yValues);
 
-        Point[] points =
-                TabulatedFunctionOperationService.asPoints(function);
+        Point[] points = TabulatedFunctionOperationService.asPoints(function);
 
         assertEquals(3, points.length);
 
@@ -29,5 +31,58 @@ class TabulatedFunctionOperationServiceTest {
 
         assertEquals(2.0, points[2].x);
         assertEquals(4.0, points[2].y);
+    }
+
+    @Test
+    void testAddArrayArray() {
+        double[] x = {1, 2, 3};
+        double[] y1 = {1, 2, 3};
+        double[] y2 = {4, 5, 6};
+
+        TabulatedFunction f1 = new ArrayTabulatedFunction(x, y1);
+        TabulatedFunction f2 = new ArrayTabulatedFunction(x, y2);
+
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService(new ArrayTabulatedFunctionFactory());
+
+        TabulatedFunction result = service.add(f1, f2);
+
+        assertEquals(3, result.getCount());
+        assertEquals(5, result.getY(0));
+        assertEquals(7, result.getY(1));
+        assertEquals(9, result.getY(2));
+    }
+
+    @Test
+    void testSubtractArrayLinkedList() {
+        double[] x = {0, 1, 2};
+        double[] y1 = {5, 6, 7};
+        double[] y2 = {1, 1, 1};
+
+        TabulatedFunction f1 = new ArrayTabulatedFunction(x, y1);
+        TabulatedFunction f2 = new LinkedListTabulatedFunction(x, y2);
+
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService(new LinkedListTabulatedFunctionFactory());
+
+        TabulatedFunction result = service.subtract(f1, f2);
+
+        assertEquals(4, result.getY(0));
+        assertEquals(5, result.getY(1));
+        assertEquals(6, result.getY(2));
+    }
+
+    @Test
+    void testInconsistentFunctionsException() {
+        double[] x1 = {1, 2, 3};
+        double[] y1 = {1, 1, 1};
+
+        double[] x2 = {1, 2};
+        double[] y2 = {2, 2};
+
+        TabulatedFunction f1 = new ArrayTabulatedFunction(x1, y1);
+        TabulatedFunction f2 = new ArrayTabulatedFunction(x2, y2);
+
+        TabulatedFunctionOperationService service = new TabulatedFunctionOperationService();
+
+        assertThrows(RuntimeException.class, () -> service.add(f1, f2));
     }
 }
