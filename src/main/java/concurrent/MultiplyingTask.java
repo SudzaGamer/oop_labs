6ff23b43c389d7268ca7d7a1b9be36672052d42e
Japan-Manul <hehhehov8@gmail.com>
2025-11-24 20:36:28 +0300

    added MultiplyingTask, MultiplyingTaskExecutor

---
+++ src/main/java/concurrent/MultiplyingTask.java
@@ -0,0 +1,22 @@
+package concurrent;
+
+import functions.TabulatedFunction;
+
+public class MultiplyingTask implements Runnable {
+
+    private final TabulatedFunction function;
+
+    public MultiplyingTask(TabulatedFunction function) {
+        this.function = function;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < function.getCount(); i++) {
+            double newY = function.getY(i) * 2;
+            function.setY(i, newY);
+        }
+
+        System.out.printf("Thread '%s' finished multiplying task%n", Thread.currentThread().getName());
+    }
+}
---
+++ src/main/java/concurrent/MultiplyingTaskExecutor.java
@@ -0,0 +1,34 @@
+package concurrent;
+
+import functions.UnitFunction;
+import functions.LinkedListTabulatedFunction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiplyingTaskExecutor {
+
+    public static void main(String[] args) throws InterruptedException {
+        UnitFunction unitFunction = new UnitFunction();
+        LinkedListTabulatedFunction function = new LinkedListTabulatedFunction(unitFunction, 1, 20, 20);
+
+        List<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            MultiplyingTask task = new MultiplyingTask(function);
+            Thread thread = new Thread(task);
+            threads.add(thread);
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        Thread.sleep(2000);
+
+        System.out.println("Final function values:");
+        for (int i = 0; i < function.getCount(); i++) {
+            System.out.printf("x = %.6f, y = %.6f%n", function.getX(i), function.getY(i));
+        }
+    }
+}
